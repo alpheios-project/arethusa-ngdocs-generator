73c3995390f3392d5fc0845faf90e513b428cff5
LFDM <1986gh@gmail.com>
2014-12-13 13:50:30 +0100

    Extract parser to own file

---
+++ parser.rb
@@ -0,0 +1,41 @@
+class Parser
+  def initialize(file)
+    @file = file
+    @lines = File.read(file).split("\n");
+  end
+
+  def annotate
+    @lines.each_with_index do |line, i|
+      return if line =~ /@ngdoc/
+      next unless @definition = get_definition(line, i)
+      break;
+    end
+
+    return unless @definition
+
+    i = @definition.line_no
+
+    unless @lines[i].end_with?('{')
+      while (next_line = @lines[i + 1]) && next_line !~ /^\s*function/
+        dependency = next_line.scan(/[\$\w]/).join
+        @definition.add_dependency(dependency)
+        i += 1
+      end
+    end
+
+    annotated_file = @definition.annotate(@lines)
+    #File.write(@file, annotated_file)
+  end
+
+  private
+
+  def get_definition(line, line_no)
+     m = line.match(def_regexp)
+     Definition.new(line_no, *m.captures) if m
+  end
+
+  def def_regexp
+    /angular\.module\('(.*)'\)\.(factory|service|directive)\('(.*)'/
+  end
+end
+
